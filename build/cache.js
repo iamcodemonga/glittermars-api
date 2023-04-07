fc32f925eab7538cb35b9382e0e741c6b44ff801
iamcodemonga <codemonga@gmail.com>
2023-04-07 01:05:04 +0100

    updated gitignore

--- build/cache.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ioredis_1 = require("ioredis");
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)();
-const url = String(process.env.REDIS_URL);
-const redis = new ioredis_1.Redis(url);
-exports.default = redis;
--- build/routes/user.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const user_1 = require("../controllers/user");
-const router = (0, express_1.Router)();
-router.get('/:id', user_1.profile);
-router.get('/orders/:userid', user_1.getOrders);
-router.post('/order', user_1.placeOrder);
-exports.default = router;
